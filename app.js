4fd2f97838d50340bcc0c1d12d2a8b0d91d9869f
prajwal zingare <prajwalzingre@gmail.com>
2022-12-22 11:05:39 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("count down app testing");
+//grt all the input filed value
+let hr = document.getElementById("hr").value;
+let min = document.getElementById("min").value;
+let sec = document.getElementById("sec").value;
